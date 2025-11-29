4a09e28074f42b6332eeabfc3823ca200c95ac2a
chaos-ina <istm.chaos@gmail.com>
2025-11-29 11:22:30 +0900

    day2完了

---
+++ day2/Sample204.java
@@ -0,0 +1,18 @@
+package day2;
+
+public class Sample204 {
+    public static void main(String[] args) {
+        int a;
+        double b, c, d, e;
+        a = (int)1.23;
+        b = 2.0;
+        c = 3;
+        d = (double)c;
+        e = (double)3;
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+        System.out.println("d = " + d);
+        System.out.println("e = " + e); 
+    }   
+}
